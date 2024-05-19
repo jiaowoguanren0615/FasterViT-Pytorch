d26255e54d08f2dd640648c9df9e167c1825671a
Huangshuqi <1105374939@qq.com>
2024-05-19 17:03:47 +1000

    Upload FasterViT

---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from .build_models import faster_vit_0_224, faster_vit_1_224, faster_vit_2_224, faster_vit_3_224, faster_vit_4_224, \
+    faster_vit_5_224, faster_vit_6_224, faster_vit_4_21k_224, faster_vit_4_21k_384, faster_vit_4_21k_512, \
+    faster_vit_4_21k_768
